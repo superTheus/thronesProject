cea25feaeed40c2e9a49c394d635698ec972820e
Matheus Souza <j.matheussouza2019@gmail.com>
2022-06-05 21:24:37 -0400

    fix: mudanda no script de build

--- src/components/load.tsx
+++ src/components/load.tsx
@@ -10,7 +10,7 @@ export default function Load() {
 
 const View = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
